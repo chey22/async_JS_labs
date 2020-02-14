e528895023282bc16e8f39a2af6154b6c7686c0b
chey22 <59664325+chey22@users.noreply.github.com>
2020-02-14 10:58:50 -0600

    Add files via upload

--- promising_results/updated-math.js
+++ promising_results/updated-math.js
@@ -1,48 +1,60 @@
-// Promise.then() Chaining
+// // Promise.then() Chaining
 
-// first use values 6 and 2
+// // first use values 6 and 2
 
-// if you change the values to 1 and 1, the .catch logs the error to the console because each math function checks for a negative number
+// // if you change the values to 1 and 1, the .catch logs the error to the console because each math function checks for a negative number
 
-slowMath.add(6, 2).then((value) => {
-    console.log(value);
-    return slowMath.multiply(value, 2)
-}).then((value) => {
-    console.log(value);
-    return slowMath.divide(value, 4)
-}).then((value) => {
-    console.log(value);
-    return slowMath.subtract(value, 3);
-}).then((value) => {
-    console.log(value);
-    return slowMath.add(value, 98);
-}).then((value) => {
-    console.log(value);
-    return slowMath.remainder(value, 2)
-}).then((value) => {
-    console.log(value);
-    return slowMath.multiply(value, 50)
-}).then((value) => {
-    console.log(value);
-    return slowMath.remainder(value, 40)
-}).then((value) => {
-    console.log(value);
-    return slowMath.add(value, 32)
-}).then((value) => {
-    console.log(value);
-    return console.log(`The final reult is ${value}`)
-}).catch((e) => {
-    return console.log(e)
-})
+// slowMath.add(6, 2)
+//     .then((val) => {
+//         console.log(val);
+//         return slowMath.multiply(val, 2)
+//     })
+//     .then((val) => {
+//         console.log(val);
+//         return slowMath.divide(val, 4)
+//     })
+//     .then((val) => {
+//         console.log(val);
+//         return slowMath.subtract(val, 3);
+//     })
+//     .then((val) => {
+//         console.log(val);
+//         return slowMath.add(val, 98);
+//     })
+//     .then((val) => {
+//         console.log(val);
+//         return slowMath.remainder(val, 2)
+//     })
+//     .then((val) => {
+//         console.log(val);
+//         return slowMath.multiply(val, 50)
+//     })
+//     .then((val) => {
+//         console.log(val);
+//         return slowMath.remainder(val, 40)
+//     })
+//     .then((val) => {
+//         console.log(val);
+//         return slowMath.add(val, 32)
+//     })
+//     .then((val) => {
+//         console.log(val);
+//         return console.log(`The final reult is ${val}`)
+//     })
+//     .catch((error) => {
+//         return console.log(error)
+//     })
 
 
 // Async/Await in ES6
 
 doMath = async () => {
     try {
-        result = await slowMath.add(6, 2)
+        let result = await slowMath.add(6, 2)
         console.log(result);
-        result = await slowMath.multiply(result, 4)
+        result = await slowMath.multiply(result, 2)
+        console.log(result)
+        result = await slowMath.divide(result, 4);
         console.log(result)
         result = await slowMath.subtract(result, 3)
         console.log(result)
@@ -55,7 +67,7 @@ doMath = async () => {
         result = await slowMath.remainder(result, 40)
         console.log(result)
         result = await slowMath.add(result, 32)
-        console.log(result)
+        console.log(`The final reult is ${result}`)
     } catch (error) {
         return console.log(error)
     }
